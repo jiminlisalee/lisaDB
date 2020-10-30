c7875b18f45fbbfbdc425827b01504f27623bd33
jiminlisalee <jmllee90@gmail.com>
2020-10-30 13:13:32 +0000

    test

---
+++ midTerm/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title> 넷플릭스 </title>
+</head>
+
+<body>
+    <h1> NETFLIX TV show / Movie </h1>
+    
+    (1)<a href="release_year.php"> 방송연도별 조회</a><br>
+    (2)<a href="korea_tvshow"> 한국 tv show 조회</a><br>
+    (3)<a href="korea_movie"> 한국 movie 조회</a><br>
+    
+    <form action="salary_info.php" method="GET">
+        (4) 제목별로 조회:
+        <input type="text" name="tile" placeholder="프로그램을 입력해주세요!">
+        <input type="submit" value="Search">
+
+    </form>
+
+    <form action="salary_info.php" method="GET">
+        (5) 국가별로 조회:
+        <input type="text" name="tile" placeholder="국가를 입력해주세요!">
+        <input type="submit" value="Search">
+
+    </form>
+    
+    
+        
+
+    
+
+</body>
+
+</html>
